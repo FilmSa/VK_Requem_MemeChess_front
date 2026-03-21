24d90e2b3108695c9c1551bb85aec75482162bee
Diwix6 <multifora.menu@gmail.com>
2026-03-21 19:45:38 +0300

    перенес панель ходов в отдельный организм

--- src/components/organisms/ChessBoardSection.jsx
+++ src/components/organisms/ChessBoardSection.jsx
@@ -1,14 +1,14 @@
 import PlayerPanel from "../molecules/PlayerPanel.jsx";
 import GameBoard from "../../features/chess/components/GameBoard.jsx";
-import MoveHistoryPanel from "../molecules/MoveHistoryPanel.jsx";
 import { useBoardScale } from "../../features/chess/hooks/useBoardScale.js";
 import { useChessGame } from "../../features/chess/hooks/useChessGame.js";
 import { useGameSocket } from "../../features/chess/hooks/useGameSocket.js";
 import { BOARD_SIZE, DEFAULT_AVATAR } from "../../features/chess/lib/boardConfig.js";
 
-export default function ChessBoardSection() {
+export default function ChessBoardSection({ gameState }) {
   const { scale, boardWidth } = useBoardScale(BOARD_SIZE);
 
+  const gameStateLocal = gameState || useChessGame();
   const {
     game,
     highlightedSquares,
@@ -16,7 +16,7 @@ export default function ChessBoardSection() {
     onSquareClick,
     onPieceDrop,
     applyRemoteMove,
-  } = useChessGame();
+  } = gameStateLocal;
 
   const { sendMove } = useGameSocket({
     onRemoteMove: applyRemoteMove,
@@ -63,10 +63,6 @@ export default function ChessBoardSection() {
             />
           </div>
         </section>
-
-        <div className="flex-shrink-0" style={{ width: "220px", height: Math.floor(BOARD_SIZE * scale), marginTop: "54px" }}>
-          <MoveHistoryPanel history={game.history()} height="450px" />
-        </div>
       </div>
       </div>
     // </div>
--- src/pages/PlayPage.jsx
+++ src/pages/PlayPage.jsx
@@ -1,16 +1,22 @@
 import Sidebar from "../components/organisms/Sidebar.jsx";
 import ChessBoardSection from "../components/organisms/ChessBoardSection";
+import GameSettingsPanel from "../components/organisms/GameSettingsPanel.jsx";
+import { useChessGame } from "../features/chess/hooks/useChessGame.js";
+
 
 
 export default function PlayPage() {
+  const chessGameState = useChessGame();
+
   return (
     <div className="w-screen h-screen overflow-hidden text-white bg-[linear-gradient(312deg,#0b0f2b_0%,#2b1a58_13.94%,#3b1f6a_29.33%,#1a1446_50.47%,#341d5b_68.17%,#1f1852_88.46%,#0b0f2b_100%)]">
       <div className="w-full h-full flex overflow-hidden">
         <Sidebar />
-        <main className="flex-1 h-full overflow-hidden flex items-center">
+        <main className="flex-1 h-full overflow-hidden flex items-start gap-4">
             <div className="ml-[50px]">
-                <ChessBoardSection />
+                <ChessBoardSection gameState={chessGameState} />
             </div>
+            <GameSettingsPanel history={chessGameState.game.history()} height="450px" style={{ marginTop: "78px" }}/>
         </main>
       </div>
     </div>
